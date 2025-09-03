7022a0ee76f4a913aaf53a0e0ed5b80fcb8556fb
JossueTorresDev <jheferson.torres.h@vallegrande.edu.pe>
2025-09-03 16:50:24 -0500

    Fresh

---
+++ prueba/src/main/java/pe/edu/vallegrande/prueba/JossueApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JossueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JossueApplication.class, args);
+	}
+
+}
